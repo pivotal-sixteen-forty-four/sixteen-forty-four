b73f50b5ecc80670e15887c42e35dff774d786af
Corey Downing <downing.c@gmail.com>
2016-03-01 09:38:28 -0700

    Initial commit
    - rails new (5.0.0 beta 3)
    - Show cards for /
    - Styling for card Hero portion
      - brand logo
      - location

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'cards/index'
+
+  root 'cards#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Serve websocket cable requests in-process
+  # mount ActionCable.server => '/cable'
+end
