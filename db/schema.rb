908a156f4fe877e8066f90aeb45030c7da032d13
TJ Taylor <dugancathal@gmail.com>
2016-03-23 09:13:57 -0600

    Make admin emails unique

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160322215338) do
+ActiveRecord::Schema.define(version: 20160323150952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20160322215338) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
