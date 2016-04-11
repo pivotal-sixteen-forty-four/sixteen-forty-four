ee5220ba8baa78a4f3a239f15bda9ab9f547f498
Alex Welch <me@alexwelch.com>
2016-04-11 13:25:34 -0600

    Add ability for @galvanize.com and @npi.com to register

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
     ENV["GOOGLE_CLIENT_ID"],
     ENV["GOOGLE_CLIENT_SECRET"],
-    hd: %w(pivotal.io galvanize.it)
+    hd: %w(pivotal.io galvanize.it galvanize.com nicholspartnership.com)
 end
